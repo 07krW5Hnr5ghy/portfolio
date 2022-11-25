d87f5c2c5e66477dccecb33e1fdb0bfb559ae1dc
Juan Pablo Romero <jryn145@gmail.com>
2022-11-24 19:44:55 -0500

    initial css + sass setup

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,8 +1,11 @@
+import photo from '../assets/profile.png';
+
 const Home = () => {
     return(
         <div id="Home_container">
             <h1>Juan Pablo Romero</h1>
             <h3>Fullstack Developer</h3>
+            <img src={photo} alt="" width={100} height={120}/>
         </div>
     );
 };
