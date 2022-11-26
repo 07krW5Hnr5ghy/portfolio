ba572d6c1e0410a9085c7bcc0c60301a67cc35db
Juan Pablo Romero <jryn145@gmail.com>
2022-11-26 18:35:48 -0500

    base css

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,23 @@ function App() {
   return (
     <div className="grid">
       <aside className="grid__sidebar">
-        <div className='grid__widget'>
+        <div className='grid__widget green'>
           <section id="home">
             <Home/>
           </section>
         </div>
-        <div className='grid__widget'>
+        <div className='grid__widget magenta'>
           <section id="about">
             <About/>
           </section>
         </div>
-        <div className='grid__widget'>
+        <div className='grid__widget magenta'>
           <section id="skills">
            <Skills/>
           </section>
         </div>
       </aside>
-      <main className='grid__main'>
+      <main className='grid__main magenta'>
         <section id="projects">
           <Projects/>
         </section>
