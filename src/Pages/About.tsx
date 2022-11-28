fe028e76ce1af4c984cec025e6ce15921f50adcd
Juan Pablo Romero <jryn145@gmail.com>
2022-11-27 20:33:59 -0500

    new space of paragraph about text

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -5,7 +5,10 @@ const About = () => {
             <p>Help-desk agent with 3 years and 9 months of work experience in Point of Sale systems support.
             <br/>
             <br/>
-            I am interested in the technologies currently used on the internet, and their inner workings. Now, I am improving my technological skills, and learning new technologies to further my career and projects.
+            I am interested in the technologies currently used on the internet, and their inner workings. 
+            <br/>
+            <br/>
+            Now, I am improving my technological skills, and learning new technologies to further my career and projects.
             </p>
         </div>
     );
