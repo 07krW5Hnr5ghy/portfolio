723431a0524d2ea5807f1d79c53d2e1d0cafef17
Juan Romero <jryn145@gmail.com>
2023-03-07 12:35:39 -0500

    fix(imports):delete unused imports in app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
 import Home from './Pages/Home';
-import About from './Pages/About';
-import Projects from './Pages/Projects';
-import Skills from './Pages/Skills';
-import Contact from './Pages/Contact';
-import Project from './components/Project';
 import Detail from "./Pages/Detail";
 import {Route,Routes} from "react-router-dom";
-import slides from './slides';
-import { Slide } from './interfaces';
 
 function App() {
   return (
